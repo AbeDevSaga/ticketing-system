7daad88d4af347541e6bc4236b9ca6a3c78d4b6e
AbeDevSaga <2000amandanm@gmail.com>
2025-03-01 20:54:05 +0300

    server initalization

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const ticketRoutes = require("./routes/ticketRoutes");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+connectDB();
+
+app.use("/api/auth", authRoutes);
+app.use("/api", ticketRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
